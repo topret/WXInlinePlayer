80269b0833f036e92b13af6d01236549b70edd04
115519417 <115519417@qq.com>
2021-09-15 09:56:59 +0800

    暂不编译 baseline

--- lib/codec/build.sh
+++ lib/codec/build.sh
@@ -6,20 +6,20 @@ rm -rf ./build
 mkdir ./build
 cd ./build
 
-# baseline decode library
-node ../tool/compile.js wasm baseline
-emcmake cmake ..
-emmake make -j 4
-mv ../bin/prod.js ../bin/baseline.wasm.js
-
-node ../tool/compile.js asm baseline
-emcmake cmake ..
-emmake make -j 4
-mv ../bin/prod.js ../bin/baseline.asm.js
-
-node ../tool/compile.js
-node ../tool/wrapper.js ../bin/baseline.wasm.js baseline.wasm
-node ../tool/wrapper.js ../bin/baseline.asm.js baseline.asm
+# baseline decode library  暂不编译 baseline
+#node ../tool/compile.js wasm baseline
+#emcmake cmake ..
+#emmake make -j 4
+#mv ../bin/prod.js ../bin/baseline.wasm.js
+#
+#node ../tool/compile.js asm baseline
+#emcmake cmake ..
+#emmake make -j 4
+#mv ../bin/prod.js ../bin/baseline.asm.js
+#
+#node ../tool/compile.js
+#node ../tool/wrapper.js ../bin/baseline.wasm.js baseline.wasm
+#node ../tool/wrapper.js ../bin/baseline.asm.js baseline.asm
 
 # all decode library
 node ../tool/compile.js wasm all
