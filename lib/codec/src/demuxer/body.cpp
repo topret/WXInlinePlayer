2d94c2c2d13cb30269732882bb2a29384983fc1a
115519417 <115519417@qq.com>
2021-09-15 14:05:44 +0800

    增加日志

--- lib/codec/src/demuxer/body.cpp
+++ lib/codec/src/demuxer/body.cpp
@@ -191,5 +191,8 @@ shared_ptr<BodyValue> Body::decode(shared_ptr<Buffer> &buffer) {
 #endif
 
   value->buffer = buffer;
+
+  printf("read nvr frame sig:0x%x, codec:%d, frameset:%d,gapms:%d,frameLen:%d, time:%d,headerSize:%d\r\n", pHead->headSig,
+	  pHead->codect, pHead->framet, pHead->gapms, pHead->framelen, retValue.timestamp, nHeaderSize);
   return value;
 }
